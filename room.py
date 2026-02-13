7134a5b4af7be515aa1ff818b48804656647b27c
emswilerkeller30 <emswilerkeller30@gmail.com>
2026-02-13 18:43:23 -0500

    function now saves to file, not file in memory

--- room.py
+++ room.py
@@ -1,7 +1,8 @@
 import scheduler
+import json
 
 #get CombinedConfig file, get the config field(SchedulerConfig type), to add room to
-loadedConfig = scheduler.load_config_from_file(scheduler.CombinedConfig, "example.json").config
+loadedConfig = scheduler.load_config_from_file(scheduler.CombinedConfig, "example.json")
 
 def main():
     addRoom()
@@ -11,14 +12,27 @@ def addRoom():
     #creates room
     newRoom = genRoom()
     #checks existing, adds to config if it does not exist already
+    #
     if not(existCheck(newRoom)):
-        with loadedConfig.edit_mode() as editableConfig:
-            try:
-                editableConfig.rooms.append(newRoom)
-                print("Room added")
-            except ValueError:
-                print("failed to add room, try again")
-                addRoom()
+        try:
+            with loadedConfig.edit_mode() as editableConfig:
+                #saves to the config loaded in memory
+                editableConfig.config.rooms.append(newRoom)
+            #writes to the actual config file, saving for later use
+            with open("example.json","r") as f:
+                data = json.load(f)
+            data["config"]["rooms"] = loadedConfig.config.rooms
+            with open("example.json","w") as f:
+                json.dump(data,f,indent=4)
+            #feedback
+            print("Room added")
+        except ValueError:
+            print("failed to add room ")
+    else:
+        print("room already exists, do not add")
+        if(promptAddRoom()):
+            addRoom()
+            
 
 #combines the prompts to create the Room string
 def genRoom():
@@ -39,13 +53,19 @@ def promptRoomBuilding():
 #returns true if it already exists
 #returns false if it does not exist
 def existCheck(room: scheduler.Room):
-    for element in loadedConfig.rooms:
+    for element in loadedConfig.config.rooms:
         print("checking element: " + element)
         print("does match: " + room +"?")
         if(element == room):
             print("yes match")
             return True
         print("no match")
+    return False
+
+def promptAddRoom():
+    answer = input("Add another room?(y/n)")
+    if (answer == "y"):
+        return True
     return False
 if __name__ == "__main__":
     main()

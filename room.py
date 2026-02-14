dc3c85f12118a88647a1c8cccaeef942d5b9b67d
emswilerkeller30 <emswilerkeller30@gmail.com>
2026-02-13 19:04:45 -0500

    addRoom can take a parameter to skip user prompt

--- room.py
+++ room.py
@@ -8,11 +8,12 @@ def main():
     addRoom()
 
 #adds room to list in config
-def addRoom():
-    #creates room
-    newRoom = genRoom()
+#you can pass a room in the parameter to add the object instead of prompting user input
+def addRoom(newRoom = None):
+    #creates room if none was passed into parameter
+    if newRoom is None:
+        newRoom = genRoom()
     #checks existing, adds to config if it does not exist already
-    #
     if not(existCheck(newRoom)):
         try:
             with loadedConfig.edit_mode() as editableConfig:
@@ -30,8 +31,8 @@ def addRoom():
             print("failed to add room ")
     else:
         print("room already exists, do not add")
-        if(promptAddRoom()):
-            addRoom()
+    if(promptAddRoom()):
+        addRoom()
             
 
 #combines the prompts to create the Room string
@@ -40,26 +41,33 @@ def genRoom():
     return promptRoomBuilding() + " " + promptRoomNumber()
 
 #prompts the room number
+#will only return if it is a number
 #returns string
 def promptRoomNumber():
-    return input("roomNum?: ")
+    roomNum = input("roomNum?: ").strip()
+    if (roomNum.isdigit()):
+        return roomNum
+    else:
+        print("not a number, try again")
+        return promptRoomNumber()
+    
 #prompts the room building
-#make sure building is capitalized
+#make sure building is capitalized and no extra whitespace
 #returns string
 def promptRoomBuilding():
-    return input("building?: ").capitalize()
+    return input("building?: ").strip().capitalize()
 
 #checks if room already exists
 #returns true if it already exists
 #returns false if it does not exist
 def existCheck(room: scheduler.Room):
     for element in loadedConfig.config.rooms:
-        print("checking element: " + element)
-        print("does match: " + room +"?")
+        #print("checking element: " + element)
+        #print("matches: " + room +"?")
         if(element == room):
-            print("yes match")
+            #print("yes match")
             return True
-        print("no match")
+        #print("no match")
     return False
 
 def promptAddRoom():
